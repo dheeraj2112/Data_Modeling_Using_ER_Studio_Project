ef75f91781d0e9529ac0ea4fa05de7bc56390300
DHEERAJ SINGH <31558540+dheeraj2112@users.noreply.github.com>
2024-10-09 20:21:41 +0530

    Delete CRON_schedule.sql

--- CRON_schedule.sql
+++
@@ -1,8 +0,0 @@
-# __________ minute (0-59)
-# | ________ hour (0-23)
-# | | ______ day of month (1-31, or L)
-# | | | ____ month (1-12, JAN-DEC)
-# | | | | _ day of week (0-6, SUN-SAT, or L)
-# | | | | |
-# | | | | |
-  * * * * *
